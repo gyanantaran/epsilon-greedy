5c325717d1f772f92aa2e077d6d72f77a112949e
Vishal Paudel <95016059+gyanantaran@users.noreply.github.com>
2024-02-28 22:52:30 +0530

    ✍️ Changed the format of the history to have tuple of action-rewards

--- multiarm_bandit/bandit.py
+++ multiarm_bandit/bandit.py
@@ -19,22 +19,22 @@ class Bandit:
 
 
 def run(bandit, N_tries):
-    history = []  # empty total history
+    history: List[Tuple] = []  # empty total history
+    
 
     arms_history = {arm: [] for arm in bandit.arms}  # empty history
     
     while( N_tries > 0 ):
-        arm_choice_index	= int(input("Enter an integer choice: "))
-        arm_choice  		= bandit.arms[ arm_choice_index ]				# expects index of the arm from the User
-        pull_result      	= pull( arm_choice )
-    
-        
-        history.append( pull_result )  # updating total history
-    
-        
-        arms_history[ arm_choice ].append( pull_result ) # updating coin history
-        
         N_tries -= 1
+        
+        arm_choice_index	= int(input("Enter an integer choice: "))
+        arm_choice  		= bandit.arms[ arm_choice_index ]			# expects index of the arm from the User
+        pull_result      	= pull( arm_choice )						# rewards/returns/result
+
+        history.append( (arm_choice, pull_result) )  # updating total history
+
+        arms_history[ arm_choice ].append( pull_result ) # updating coin history
+
         display_info(remaining_tries=N_tries, arms_history=arms_history)  # !passing the WHOLE history in
         
 
